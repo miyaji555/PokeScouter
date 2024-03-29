40654c139a76f91b2baf6f77708817eb973693ea
miyasic <kou.sepak@gmail.com>
2024-03-29 22:37:52 +0900

    ProgressIndicatorThemeDataを指定

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,38 +4,42 @@ import 'package:poke_scouter/constants/provider_name.dart';
 
 final themeProvider = Provider.family<ThemeData, Brightness>((ref, brightness) {
   final theme = ThemeData(
-      brightness: brightness,
-      primaryColor: Colors.teal,
-      primarySwatch: Colors.teal,
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.teal,
-        foregroundColor: Color(0xFF003333),
+    brightness: brightness,
+    primaryColor: Colors.teal,
+    primarySwatch: Colors.teal,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.teal,
+      foregroundColor: Color(0xFF003333),
+    ),
+    tabBarTheme: const TabBarTheme(
+      labelColor: Color(0xFF808000),
+      unselectedLabelColor: Colors.white60,
+      indicatorColor: Color(0xFF808000),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Color(0xFF003333),
+      backgroundColor: Colors.teal,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateColor.resolveWith((states) =>
+            states.contains(MaterialState.disabled)
+                ? const Color(0xFF446666)
+                : const Color(0xFF003333)),
+        backgroundColor: MaterialStateColor.resolveWith((states) =>
+            states.contains(MaterialState.disabled)
+                ? const Color(0xAF668080)
+                : const Color(0xFF009999)),
+        // backgroundColor: MaterialStateProperty.all(const Color(0xFF009999)),
       ),
-      tabBarTheme: const TabBarTheme(
-        labelColor: Color(0xFF808000),
-        unselectedLabelColor: Colors.white60,
-        indicatorColor: Color(0xFF808000),
-      ),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        foregroundColor: Color(0xFF003333),
-        backgroundColor: Colors.teal,
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateColor.resolveWith((states) =>
-              states.contains(MaterialState.disabled)
-                  ? const Color(0xFF446666)
-                  : const Color(0xFF003333)),
-          backgroundColor: MaterialStateColor.resolveWith((states) =>
-              states.contains(MaterialState.disabled)
-                  ? const Color(0xAF668080)
-                  : const Color(0xFF009999)),
-          // backgroundColor: MaterialStateProperty.all(const Color(0xFF009999)),
-        ),
-      ),
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        selectedItemColor: Colors.teal,
-      ));
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedItemColor: Colors.teal,
+    ),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: Colors.teal,
+    ),
+  );
   return theme.copyWith(
       colorScheme: theme.colorScheme.copyWith(secondary: Colors.lime));
 }, name: kProviderNameTheme);
