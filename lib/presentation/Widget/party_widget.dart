f8040fad5895fff415a6f6ded134599778f760d8
miyasic <kou.sepak@gmail.com>
2024-03-29 23:13:11 +0900

    不要なWidgetを削除

--- lib/presentation/Widget/party_widget.dart
+++ lib/presentation/Widget/party_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:poke_scouter/constants/text_style.dart';
 import 'package:poke_scouter/domain/firebase/party.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
