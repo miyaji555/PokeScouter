40442f72fbb0711f1cec816482d29103690976e3
miyasic <kou.sepak@gmail.com>
2024-05-14 22:27:31 +0900

    annotationをyolo形式からjson形式に変更

--- python/image/training/object_detection/src/check_annotaion.py
+++ python/image/training/object_detection/src/check_annotaion.py
@@ -1,41 +1,57 @@
-import json
 from PIL import Image, ImageDraw
+import os
 import sys
+import pandas as pd
 
 # モジュールがあるディレクトリを追加
 sys.path.append('../..')
 
 from util import clear_directory, create_recursive_dir
 
-def draw_bounding_boxes(image_path, annotations, output_path):
+def draw_bounding_boxes(image_path, label_path, output_path, classes):
     # 画像を読み込む
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
+    img_width, img_height = image.size
 
-    # アノテーションごとにバウンディングボックスを描画
-    for annotation in annotations:
-        xmin = annotation['xmin']
-        ymin = annotation['ymin']
-        xmax = annotation['xmax']
-        ymax = annotation['ymax']
+    # ラベルファイルを読み込む
+    with open(label_path, 'r') as f:
+        labels = f.readlines()
+
+    # ラベルごとにバウンディングボックスを描画
+    for label in labels:
+        class_id, x_center, y_center, width, height = map(float, label.split())
+        x_center *= img_width
+        y_center *= img_height
+        width *= img_width
+        height *= img_height
+        xmin = x_center - width / 2
+        ymin = y_center - height / 2
+        xmax = x_center + width / 2
+        ymax = y_center + height / 2
         draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="red", width=3)
+        draw.text((xmin, ymin), classes[int(class_id)], fill="red")
 
     # 画像を保存または表示
     image.save(output_path)
 
-# アノテーションファイルの読み込み
-with open('../output/annotations.json', 'r') as file:
-    annotations_by_image = json.load(file)
+# クラス名のリストを取得
+spreadsheet_path = '../../学習ラベル.csv'
+df = pd.read_csv(spreadsheet_path)
+classes = list(set(df.poke_name.values))
 
-
+# 出力ディレクトリのパス
 output_annotation_path = "../output"
-
-checkd_image_path = "../output/checked"
-clear_directory(checkd_image_path)
-create_recursive_dir(checkd_image_path)
+checked_image_path = "../output/checked"
+clear_directory(checked_image_path)
+create_recursive_dir(checked_image_path)
 
 # 各画像に対してバウンディングボックスを描画
-for image_filename, annotations in annotations_by_image.items():
-    image_path = f'../output/image/{image_filename}'
-    output_path = f'../output/checked/{image_filename}'
-    draw_bounding_boxes(image_path, annotations, output_path)
+image_dir = os.path.join(output_annotation_path, 'image')
+label_dir = os.path.join(output_annotation_path, 'labels')
+for image_filename in os.listdir(image_dir):
+    if image_filename.endswith('.png'):
+        image_path = os.path.join(image_dir, image_filename)
+        label_path = os.path.join(label_dir, image_filename.replace('.png', '.txt'))
+        output_path = os.path.join(checked_image_path, image_filename)
+        draw_bounding_boxes(image_path, label_path, output_path, classes)
--- python/image/training/object_detection/src/data_set.py
+++ python/image/training/object_detection/src/data_set.py
@@ -5,7 +5,8 @@ import sys
 from collections import defaultdict
 import math
 import json
-
+import os
+from tqdm import tqdm
 
 # モジュールがあるディレクトリを追加
 sys.path.append('../..')
@@ -13,42 +14,24 @@ sys.path.append('../..')
 from util import clear_directory, create_recursive_dir
 
 def apply_affine_transformation(image):
-    # 元の画像サイズ
     original_width, original_height = image.size
-
-    # 歪み前の画像をキャンバスに配置
     canvas = Image.new('RGBA', (original_width, original_height), (255, 255, 255, 0))
     canvas.paste(image, (0, 0))
 
-    angle = random.randint(0, 360)  # 回転角度
-    # 回転の中心を画像の中心に設定
+    angle = random.randint(0, 360)
     center_x = canvas.width / 2
     center_y = canvas.height / 2
 
-    scale_x = random.uniform(0.8, 1.2)  # X軸の拡大縮小率
-    scale_y = random.uniform(0.8, 1.2)  # Y軸の拡大縮小率
+    scale_x = random.uniform(0.8, 1.2)
+    scale_y = random.uniform(0.8, 1.2)
     scaled_width = int(original_width * scale_x)
     scaled_height = int(original_height * scale_y)
-    shear = random.uniform(-0.1, 0.1)  # 歪みの強さ
+    shear = random.uniform(-0.1, 0.1)
 
-    canvas = canvas.rotate(
-        angle,
-        resample=Image.BICUBIC,
-        center=(center_x, center_y)
-    )
-
-    # 画像を拡大縮小
-    canvas = canvas.resize(
-        (scaled_width, scaled_height),
-        resample=Image.BICUBIC
-    )
-    shear = random.uniform(-0.1, 0.1)  # 歪みの強さ
-    canvas = canvas.transform(
-        (scaled_width, scaled_height),
-        Image.AFFINE,
-        (1, shear, 0, 0, 1, 0),
-        Image.Resampling.BICUBIC
-    )
+    canvas = canvas.rotate(angle, resample=Image.BICUBIC, center=(center_x, center_y))
+    canvas = canvas.resize((scaled_width, scaled_height), resample=Image.BICUBIC)
+    shear = random.uniform(-0.1, 0.1)
+    canvas = canvas.transform((scaled_width, scaled_height), Image.AFFINE, (1, shear, 0, 0, 1, 0), Image.Resampling.BICUBIC)
 
     return canvas
 
@@ -66,9 +49,9 @@ df = pd.read_csv(spreadsheet_path)
 
 # ファイル名とポケモン名の辞書を作成
 pokemon_names = pd.Series(df.poke_name.values, index=df.filename).to_dict()
+classes = list(set(pokemon_names.values()))  # クラス名のリストを生成
 
 def is_overlapping(box1, box2):
-    # 各ボックスは(xmin, ymin, xmax, ymax)形式
     return not (box1[2] < box2[0] or box1[0] > box2[2] or box1[3] < box2[1] or box1[1] > box2[3])
 
 def initialize_appearance_matrix(num_pokemon, num_repeats):
@@ -77,19 +60,19 @@ def initialize_appearance_matrix(num_pokemon, num_repeats):
 def select_pokemon_for_image(appearance_matrix, pokemon_files):
     flat_list = [(i, j) for i in range(len(appearance_matrix)) for j in range(len(appearance_matrix[i])) if not appearance_matrix[i][j]]
     if not flat_list:
-        return None  # すべてのポケモンが指定回数登場していれば終了
+        return None
     selected_index = random.choice(flat_list)
     pokemon_index, repeat_index = selected_index
     return pokemon_index, repeat_index
 
-def get_non_overlapping_box(background_size, existing_boxes, icon_size, max_attempts=50):
+def get_non_overlapping_box(background_size, existing_boxes, icon_size, max_attempts):
     for _ in range(max_attempts):
         x = random.randint(0, background_size[0] - icon_size[0])
         y = random.randint(0, background_size[1] - icon_size[1])
         new_box = (x, y, x + icon_size[0], y + icon_size[1])
         if all(not is_overlapping(new_box, box) for box in existing_boxes):
             return new_box
-    return None  # 適切なボックスが見つからない場合はNoneを返す
+    return None
 
 def create_image_with_pokemons(background_size, pokemon_files, appearance_matrix, num_pokemons=6, image_index=0):
     background = Image.new('RGB', background_size, (255, 255, 255))
@@ -103,7 +86,7 @@ def create_image_with_pokemons(background_size, pokemon_files, appearance_matrix
         pokemon_index, repeat_index = result
         pokemon_file = pokemon_files[pokemon_index]
         pokemon_icon = Image.open(input_folder_path + '/' + pokemon_file)
-        pokemon_icon = apply_affine_transformation(pokemon_icon)  # 画像に変形を適用
+        pokemon_icon = apply_affine_transformation(pokemon_icon)
         pokemon_name = pokemon_names[pokemon_file]
 
         icon_size = (pokemon_icon.width, pokemon_icon.height)
@@ -128,9 +111,8 @@ def create_image_with_pokemons(background_size, pokemon_files, appearance_matrix
 
     return background, annotations
 
-
-num_pokemon = len(pokemon_names)  # ポケモンの総数
-num_repeats = 300  # 各ポケモンが登場すべき回数
+num_pokemon = len(pokemon_names)
+num_repeats = 10
 appearance_matrix = initialize_appearance_matrix(num_pokemon, num_repeats)
 annotations_by_image = defaultdict(list)
 num_pokemons_per_image = 6
@@ -138,16 +120,33 @@ num_pokemons_per_image = 6
 print(f'{num_pokemon} 匹のポケモンを {num_repeats} 回ずつ登場させる。1画像につき {num_pokemons_per_image} 匹ずつであるため、{math.ceil(num_pokemon * num_repeats / num_pokemons_per_image)} 枚の画像を生成')
 
 i = 0
-while any(not all(row) for row in appearance_matrix): 
-
+while any(not all(row) for row in appearance_matrix):
     image_filename = f'image_{i}.png'
-    image, annotations = create_image_with_pokemons((800, 600), df.filename.tolist(),appearance_matrix, num_pokemons_per_image, image_index=i)
+    image, annotations = create_image_with_pokemons((800, 600), df.filename.tolist(), appearance_matrix, num_pokemons_per_image, image_index=i)
     image.save(f'{output_image_path}/{image_filename}')
     for annotation in annotations:
         annotation['filename'] = image_filename
         annotations_by_image[image_filename].append(annotation)
     i += 1
 
-# JSON形式で全アノテーションを保存
-with open(f'{output_annotation_path}/annotations.json', 'w') as f:
-    json.dump(annotations_by_image, f, indent=4, ensure_ascii=False)
+# YOLO形式に変換
+os.makedirs(f'{output_annotation_path}/labels', exist_ok=True)
+
+for image_file, boxes in tqdm(annotations_by_image.items()):
+    label_file = os.path.join(output_annotation_path, 'labels', image_file.replace('.png', '.txt'))
+    
+    with open(label_file, 'w') as f:
+        for box in boxes:
+            class_id = classes.index(box['class'])
+            xmin, ymin, xmax, ymax = box['xmin'], box['ymin'], box['xmax'], box['ymax']
+            
+            img_path = os.path.join(output_image_path, image_file)
+            img = Image.open(img_path)
+            img_width, img_height = img.size
+            
+            x_center = (xmin + xmax) / 2.0 / img_width
+            y_center = (ymin + ymax) / 2.0 / img_height
+            width = (xmax - xmin) / img_width
+            height = (ymax - ymin) / img_height
+            
+            f.write(f"{class_id} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
