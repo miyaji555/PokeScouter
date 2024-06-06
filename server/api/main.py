0d387fdcb52e4e2c4b3f920fa235fa21dbe68ba7
miyasic <kou.sepak@gmail.com>
2024-06-07 08:03:48 +0900

    クラスidではなく、ポケモンidで返却するようにした。不要なid(10000以上)についてはレスポンスから除外した

--- server/api/main.py
+++ server/api/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI, File, UploadFile
 import torch
 from PIL import Image
 import io
+import pandas as pd
 from typing import List
 
 app = FastAPI()
+
+label_path = 'model/label-top150.csv'
+# CSVファイルの読み込み
+df = pd.read_csv(label_path)  # CSVファイル名を指定
+poke_dict = dict(zip(df['poke_name'], df['number']))
 
 # トレーニング済みモデルのロード
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='model/best.pt')
@@ -20,13 +26,16 @@ async def predict(image: UploadFile = File(...)):
 
     # 推論
     results = model(img)
-
+    
     # 検出されたクラスとIDのリストを取得
     detected_classes = results.xyxyn[0][:, -1].cpu().numpy().astype(int).tolist()
     class_names = [model.names[i] for i in detected_classes]
     
+    # クラス名をポケモンナンバーに変換
+    class_numbers = [poke_dict.get(name, 'Unknown') for name in class_names]
+
     # 結果を整理して返す
-    detections = [{'class_name': class_names[i], 'id': detected_classes[i]} for i in range(len(class_names))]
+    detections = [{'class_name': name, 'number': number} for name, number in zip(class_names, class_numbers) if number != 'Unknown' and number < 10000]
     
     response = {
         'pokemon': detections
